2b3d36932184733343a92ec6d427260463c4f42c
Dev-Sams1012 <samuel.augusto.abreu@gmail.com>
2025-11-11 18:15:50 -0300

    Definição da Interface da estrutura Cilindro, com métodos de interseção e renderização.

---
+++ Classes/Objetos/Cilindro/Cilindro.hpp
@@ -0,0 +1,22 @@
+#ifndef CILINDRO_HPP
+#define CILINDRO_HPP
+
+#include "../ObjetoAbstrato/Objeto.hpp"
+
+struct Cilindro : public Objeto
+{
+    Ponto Centro_base;
+    float Raio_base;
+    float Altura;
+    Vetor Eixo;
+    Matriz3x3 Q_Matrix;
+    Matriz3x3 M_Matrix;
+
+    Cilindro(Ponto Cb, float Rb, float H, Vetor dr, Cor Kd, Cor Ke, Cor Ka, int m_cor);
+
+    bool raioIntercepta(Ponto origem, Ponto canvas) override;
+
+    void renderiza(Cor &finalColor, Ponto origem, Ponto P_F, Cor I_F, Cor I_A) override;
+};
+
+#endif
