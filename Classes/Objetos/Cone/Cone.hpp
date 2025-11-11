5b8480988561331e58b8830827160960f4bb4739
Dev-Sams1012 <samuel.augusto.abreu@gmail.com>
2025-11-11 18:27:10 -0300

    Definição da Interface da estrutura Cone, com métodos de interseção e renderização.

---
+++ Classes/Objetos/Cone/Cone.hpp
@@ -0,0 +1,24 @@
+#ifndef CONE_HPP
+#define CONE_HPP
+
+#include "../ObjetoAbstrato/Objeto.hpp"
+
+struct Cone : public Objeto
+{
+    Ponto Centro_base;
+    Ponto Vertice_topo;
+    float Raio_base;
+    float Altura;
+    Vetor Eixo;
+    Matriz3x3 Q_Matrix;
+    Matriz3x3 M_Matrix;
+    bool TemBase;
+
+    Cone(Ponto Cb, float Rb, float H, Vetor dr, Cor Kd, Cor Ke, Cor Ka, int M, bool temBase = false);
+
+    bool raioIntercepta(Ponto origem, Ponto canvas) override;
+
+    void renderiza(Cor &finalColor, Ponto origem, Ponto P_F, Cor I_F, Cor I_A) override;
+};
+
+#endif
