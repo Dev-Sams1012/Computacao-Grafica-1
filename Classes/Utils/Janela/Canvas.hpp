2ccd6840041082d176f56c65ebe5d13bae21a2ab
Dev-Sams1012 <samuel.augusto.abreu@gmail.com>
2025-11-11 20:48:16 -0300

    Adição de suporte a iluminação e sombras, e vetor de objetos como novo atributo.

--- Classes/Utils/Janela/Canvas.hpp
+++ Classes/Utils/Janela/Canvas.hpp
@@ -1,27 +1,37 @@
 #ifndef CANVAS_HPP
 #define CANVAS_HPP
 
+#include <iostream>
+#include <fstream>
 #include <cmath>
 #include <vector>
 #include <string>
 #include "Janela.hpp"
+#include "Luz.hpp"
 #include "../Ponto/Ponto.hpp"
+#include "../Vetor/Vetor.hpp"
+#include "../../objetos/objetoabstrato/Objeto.hpp"
 
 using namespace std;
 
 struct Canvas
 {
+    Ponto origem;
     Janela janela;
     size_t nCol;
     size_t nLin;
     float Dx;
     float Dy;
     vector<vector<Cor>> imagem;
+    vector<Objeto *> objetos;
 
-    Canvas(Janela j, size_t nlin, size_t ncol);
+    Canvas(Janela j, size_t nlin, size_t ncol, Ponto origem = Ponto(0.0, 0.0, 0.0));
 
-    void GeraImagem(string nomeArquivo);
+    void adicionaObjetoCena(Objeto *obj);
+
+    bool temSombra(Ponto P_I, Luz luz, Objeto *objeto_atual);
+
+    void geraImagem(Luz luz, string nomeArquivo);
 };
-
 
 #endif
