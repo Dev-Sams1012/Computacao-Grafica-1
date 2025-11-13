b9a30a156e58698f68cc791aa45df25d8a26fe8b
Dev-Sams1012 <samuel.augusto.abreu@gmail.com>
2025-11-13 16:26:09 -0300

    Inclusão da implementação do método "temSombra" da estrutura Objeto.

---
+++ Classes/Objetos/ObjetoAbstrato/Objeto.cpp
@@ -0,0 +1,30 @@
+#include "Objeto.hpp"
+
+bool Objeto::temSombra(Ponto P_I, Luz luz, Objeto *objeto_atual, vector<Objeto *> objetos )
+{
+    Vetor L = luz.PF - P_I;
+    float distancia_Pi_Pf = sqrtf(produtoEscalar(L, L));
+    if (distancia_Pi_Pf <= 0.0f)
+        return false;
+
+    Vetor L_dir = normalizar(L);
+    float epsilon = 0.001f;
+
+    Ponto Ponto_Luz = ray(P_I, L_dir, distancia_Pi_Pf);
+
+    for (Objeto *objeto : objetos)
+    {
+        if (objeto != objeto_atual)
+        {
+            if (objeto->raioIntercepta(P_I, Ponto_Luz))
+            {
+                if (objeto->t_i > epsilon && objeto->t_i < distancia_Pi_Pf)
+                {
+                    return true;
+                }
+            }
+        }
+    }
+
+    return false;
+}
