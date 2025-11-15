6fb4cafe87d0eae30486d2c413094aae28ebfcb0
Dev-Sams1012 <samuel.augusto.abreu@gmail.com>
2025-11-15 14:08:42 -0300

    Adição de novos atributos e construtor à estrutura Plano para suporte a texturas.

--- Classes/Objetos/Plano/Plano.hpp
+++ Classes/Objetos/Plano/Plano.hpp
@@ -1,14 +1,26 @@
 #ifndef PLANO_HPP
 #define PLANO_HPP
 
+#include "../BibliotecasExternas/stb_image.hpp"
 #include "../ObjetoAbstrato/Objeto.hpp"
+
+#include <string>
+#include <iostream>
 
 struct Plano : public Objeto
 {
     Ponto P_pi;
     Vetor n_bar;
+    bool tem_textura;
+    string arquivo_textura = "";
+    unsigned char* textura = nullptr;
+    int tex_largura, tex_altura, tex_componentes;
 
     Plano(Ponto Ppi, Vetor nbar, Cor Kd, Cor Ke, Cor Ka, int M);
+
+    Plano(Ponto Ppi, Vetor nbar, string arquivoTextura, int M);
+
+    Cor texturaEm(Ponto p);
 
     bool raioIntercepta(Ponto origem, Ponto canvas) override;
 
