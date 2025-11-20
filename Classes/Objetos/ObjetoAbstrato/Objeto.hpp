6c913f7a40b7f3957233bd7c9071900f7cbcc351
Dev-Sams1012 <samuel.augusto.abreu@gmail.com>
2025-11-20 15:54:28 -0300

    Adicionadas as funções virtuais temTextura e texturaEm na estrutura Objeto para permitir suporte a texturas.

--- Classes/Objetos/ObjetoAbstrato/Objeto.hpp
+++ Classes/Objetos/ObjetoAbstrato/Objeto.hpp
@@ -20,6 +20,8 @@ struct Objeto
 
     virtual bool raioIntercepta(Ponto origem, Ponto canvas) = 0;
     virtual void renderiza(Cor &finalColor, Ponto origem, Ponto P_F, Cor I_F, Cor I_A) = 0;
+    virtual bool temTextura() const { return false; }
+    virtual Cor texturaEm(const Ponto &p) const { return K_d; }
     bool temSombra(Ponto P_I, Luz luz, Objeto *objeto_atual, vector<Objeto *> objetos);
 };
 
