cc38e20f91b5243bbbe8f3d8da4fe1f47eeccb0a
Dev-Sams1012 <samuel.augusto.abreu@gmail.com>
2025-11-20 18:05:26 -0300

    Primeiros testes da atividade 06, com a inclusão de um plano e a mesa.

---
+++ Atividade06/main.cpp
@@ -0,0 +1,57 @@
+#define _USE_MATH_DEFINES
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <cmath>
+
+#include "../Classes/utils/Janela/Janela.hpp"
+#include "../Classes/utils/Janela/Canvas.hpp"
+#include "../Classes/utils/Ponto/Ponto.hpp"
+#include "../Classes/utils/Vetor/Vetor.hpp"
+#include "../Classes/utils/Matriz/Matriz3x3.hpp"
+#include "../Classes/utils/Matriz/Matriz4x4.hpp"
+
+#include "../Classes/Objetos/Cilindro/Cilindro.hpp"
+#include "../Classes/Objetos/Malha/Malha.hpp"
+#include "../Classes/Objetos/Triangulo/Triangulo.hpp"
+#include "../Classes/Objetos/Cone/Cone.hpp"
+#include "../Classes/Objetos/Esfera/Esfera.hpp"
+#include "../Classes/Objetos/Plano/Plano.hpp"
+#include "../Classes/Objetos/Cubo/Cubo.hpp"
+#include "../Classes/Objetos/Paralelepipedo/Paralelepipedo.hpp"
+#include "ObjetosAtividade/Mesa.hpp"
+
+using namespace std;
+
+int main()
+{
+    /* origem do Observador */
+    Ponto origem = Ponto(0.0f, 0.0f, 0.0f);
+
+    /* janela Real */
+    Janela janela = Janela(0.6f, 0.6f, 0.3f);
+
+    size_t nCol = 600;
+    size_t nLin = 600;
+
+    Cor I_F = Cor(0.7f, 0.7f, 0.7f);
+    Ponto P_F = Ponto(-1.0f, 1.4f, -0.2f);
+    Cor I_A = Cor(0.3f, 0.3f, 0.3f);
+
+    Luz luz = Luz(P_F, I_F, I_A);
+
+    Canvas canvas = Canvas(janela, nLin, nCol, origem);
+
+    string pisoMadeira = "../Assets/madel-piso-vinilico-click-madeir.png";
+
+    Plano planoChao = Plano(Ponto(0.0f, -1.5f, 0.0f), Vetor(0.0f, 1.0f, 0.0f), pisoMadeira, 10);
+
+    Mesa mesa = Mesa();
+
+    canvas.adicionaObjetoCena(&planoChao);
+    canvas.adicionaObjetoCena(&mesa);
+
+    canvas.geraImagem(luz, "Imagem_Com_ObjetosComplexos");
+
+    return 0;
+}
