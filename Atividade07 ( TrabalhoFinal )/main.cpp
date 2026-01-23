e1cdeb7a50e31fd815949778291a327048737129
Dev-Sams1012 <samuel.augusto.abreu@gmail.com>
2026-01-23 14:43:44 -0300

    Atualiza cena, testando Poste.

--- Atividade07 ( TrabalhoFinal )/main.cpp
+++ Atividade07 ( TrabalhoFinal )/main.cpp
@@ -23,9 +23,11 @@
 #include "../Classes/Objetos/Cubo/Cubo.hpp"
 #include "../Classes/Objetos/Paralelepipedo/Paralelepipedo.hpp"
 
+#include "Objetos/poste.hpp"
+
 using namespace std;
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
     Ponto origem = Ponto(0.0f, 0.0f, 0.0f);
 
@@ -35,35 +37,39 @@ int main(int argc, char** argv)
     size_t nLin = 600;
 
     Cor I_F = Cor(0.7f, 0.7f, 0.7f);
-    Ponto P_F = Ponto(1.0f, 1.4f, -5.0f);
+    Ponto P_F = Ponto(0.0f, 2.0f, 2.0f);
     Cor I_A = Cor(0.6f, 0.6f, 0.6f);
 
-    LuzPontual luz = LuzPontual(P_F, I_F);
+    LuzSpot luz = LuzSpot(P_F, Vetor(0.0f, 0.0f, -5.0f), 45.0f, I_F);
+    LuzDirecional sol(Vetor(-0.3f, -0.4f, -0.2f), Cor(1.0f, 0.85f, 0.6f));
 
-    Ponto pos_cam = Ponto(0.0f, 0.0f, 0.0f);
+    Ponto pos_cam = Ponto(0.0f, 1.7f, 4.0f);
 
-    Camera* camera = new Camera(pos_cam, Ponto(0.0f, 0.0f, -1.0f), Vetor(0.0f, 1.0f, 0.0f));
+    Camera *camera = new Camera(pos_cam, Ponto(0.0f, 1.7f, -1.0f), Vetor(0.0f, 1.0f, 0.0f));
 
-    Canvas* canvas = new Canvas(janela, nLin, nCol, camera, I_A);
+    Canvas *canvas = new Canvas(janela, nLin, nCol, camera, I_A);
 
     string pisoMadeira = "../Assets/madel-piso-vinilico-click-madeir.png";
 
     string ceu = "../Assets/textura-do-ceu_14223-97.png";
-    
-    Plano planoChao = Plano(Ponto(0.0f, -2.0f, 0.0f), Vetor(0.0f, 1.0f, 0.0f), pisoMadeira, 0.2f, 10);
+
+    Plano planoChao = Plano(Ponto(0.0f, 0.0f, 0.0f), Vetor(0.0f, 1.0f, 0.0f), pisoMadeira, 0.2f, 10);
 
     Plano planoCeu = Plano(Ponto(65.0f, -5.0f, -50.0f), Vetor(0.0f, 0.0f, 1.0f), ceu, 0.007f, 10);
 
     canvas->adicionaObjetoCena(&planoChao);
     canvas->adicionaObjetoCena(&planoCeu);
 
-    canvas->adicionaLuz(&luz);
+    canvas->adicionaObjetoCena(new Poste());
+
+    //canvas->adicionaLuz(&luz);
+    canvas->adicionaLuz(&sol);
 
     canvas->geraImagem("Imagem_Trabalho_Final");
 
     Interface::inicializar(argc, argv, nCol, nLin, canvas, "Trabalho Final - Computacao Grafica 1");
 
     glutMainLoop();
-    
+
     return 0;
 }
