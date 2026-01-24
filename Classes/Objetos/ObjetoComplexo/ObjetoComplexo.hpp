76ab5aeb7c9f2cb6ea9ae5e6a6931ee8a4b84b52
Dev-Sams1012 <samuel.augusto.abreu@gmail.com>
2026-01-24 13:15:49 -0300

    Refatoração do ObjetoComplexo para uso de HitInfo e remoção de estado interno.

--- Classes/Objetos/ObjetoComplexo/ObjetoComplexo.hpp
+++ Classes/Objetos/ObjetoComplexo/ObjetoComplexo.hpp
@@ -7,21 +7,22 @@
 struct ObjetoComplexo : Objeto
 {
     vector<Objeto *> componentes;
-    Objeto *componenteQueAcertou = nullptr;
-
+    
     ObjetoComplexo();
 
     void adicionarComponente(Objeto *obj);
 
-    bool raioIntercepta(const Ponto &origem, const Vetor &Dr) override;
+    bool raioIntercepta(const Ponto &origem, const Vetor &Dr, HitInfo &hit) override;
 
-    void renderiza(Cor &finalColor, const Ponto &origem, const Vetor &Dr, const Luz &luz) const override;
+    void renderiza(Cor &finalColor, const HitInfo &hit, const Luz &luz) const override;
 
     virtual Vetor normalEm(const Ponto &P) const override;
 
     void transforma(const Matriz4x4 &M) override;
 
     bool pertenceA(const Objeto *outro) const override;
+
+    string getNomeObj() const override;
 };
 
 #endif
