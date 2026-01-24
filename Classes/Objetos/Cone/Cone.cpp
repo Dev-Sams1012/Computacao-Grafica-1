125d7e917a73ad114f0aa03158aa0f1325a85cfb
Dev-Sams1012 <samuel.augusto.abreu@gmail.com>
2026-01-24 20:15:31 -0300

    Possibilita criar tronco de cone.

--- Classes/Objetos/Cone/Cone.cpp
+++ Classes/Objetos/Cone/Cone.cpp
@@ -5,8 +5,24 @@ Cone::Cone(Ponto Cb, float Rb, float H, Vetor dr, bool temBase, Cor Kd, Cor Ke, 
     Centro_base = Cb;
     Raio_base = Rb;
     Altura = H;
+    Altura_corte = H;
     Eixo = normalizar(dr);
     TemBase = temBase;
+    TemTampaSuperior = false;
+    Vertice_topo = ray(Centro_base, Eixo, Altura);
+    Q_Matrix = (Eixo * Eixo) * (1.0f / produtoEscalar(Eixo, Eixo));
+    M_Matrix = Matriz3x3(1.0f) - Q_Matrix;
+}
+
+Cone::Cone(Ponto Cb, float Rb, float H, float H_corte, Vetor dr, bool temBase, bool temTampaSup, Cor Kd, Cor Ke, Cor Ka, int M) : Objeto(Kd, Ke, Ka, M)
+{
+    Centro_base = Cb;
+    Raio_base = Rb;
+    Altura = H;
+    Altura_corte = H_corte;
+    Eixo = normalizar(dr);
+    TemBase = temBase;
+    TemTampaSuperior = temTampaSup;
     Vertice_topo = ray(Centro_base, Eixo, Altura);
     Q_Matrix = (Eixo * Eixo) * (1.0f / produtoEscalar(Eixo, Eixo));
     M_Matrix = Matriz3x3(1.0f) - Q_Matrix;
@@ -43,7 +59,7 @@ bool Cone::raioIntercepta(const Ponto &origem, const Vetor &Dr, HitInfo &hit)
         Ponto P_I = ray(origem, Dr, r1);
         Vetor CbP = P_I - Centro_base;
         float altura = produtoEscalar(CbP, Eixo);
-        if (altura >= 0 && altura <= Altura)
+        if (altura >= 0 && altura <= Altura_corte)
         {
             t_valido = r1;
         }
@@ -54,7 +70,7 @@ bool Cone::raioIntercepta(const Ponto &origem, const Vetor &Dr, HitInfo &hit)
         Ponto P_I = ray(origem, Dr, r2);
         Vetor CbP = P_I - Centro_base;
         float altura = produtoEscalar(CbP, Eixo);
-        if (altura >= 0 && altura <= Altura)
+        if (altura >= 0 && altura <= Altura_corte)
         {
             if (t_valido < 0 || r2 < t_valido)
             {
@@ -78,13 +94,41 @@ bool Cone::raioIntercepta(const Ponto &origem, const Vetor &Dr, HitInfo &hit)
         }
     }
 
+    if (TemTampaSuperior && Altura_corte < Altura)
+    {
+        Ponto P_topo_centro = ray(Centro_base, Eixo, Altura_corte);
+        float t_s = produtoEscalar(Eixo, P_topo_centro - origem) / produtoEscalar(Eixo, Dr);
+
+        if (t_s > epsilon)
+        {
+            Ponto P_s = ray(origem, Dr, t_s);
+            float Raio_corte = Raio_base * (1.0f - (Altura_corte / Altura));
+            if (produtoEscalar(P_s - P_topo_centro, P_s - P_topo_centro) <= powf(Raio_corte, 2))
+            {
+                if (t_valido < 0 || t_s < t_valido)
+                    t_valido = t_s;
+            }
+        }
+    }
+
     if (t_valido > epsilon && t_valido < hit.t)
     {
         hit.t = t_valido;
         hit.objeto = this;
         hit.objetoRaiz = this;
         hit.ponto = ray(origem, Dr, t_valido);
-        hit.normal = normalizar(normalEm(hit.ponto));
+
+        Vetor normalCalculada = normalizar(normalEm(hit.ponto));
+
+        if (produtoEscalar(Dr, normalCalculada) > 0)
+        {
+            hit.normal = -normalCalculada;
+        }
+        else
+        {
+            hit.normal = normalCalculada;
+        }
+
         return true;
     }
 
@@ -95,9 +139,28 @@ void Cone::transforma(const Matriz4x4 &M)
 {
     Centro_base = M * Centro_base;
     Eixo = normalizar(M * Eixo);
+    Vertice_topo = ray(Centro_base, Eixo, Altura);
 }
 
 Vetor Cone::normalEm(const Ponto &P) const
 {
-    return -normalizar(P - Vertice_topo);
+    Vetor CP = P - Centro_base;
+    float h = produtoEscalar(CP, Eixo);
+
+    if (h < epsilon)
+    {
+        return -Eixo;
+    }
+
+    if (h > Altura_corte - epsilon)
+    {
+        return Eixo;
+    }
+
+    Ponto P_eixo = ray(Centro_base, Eixo, h);
+    Vetor radial = normalizar(P - P_eixo);
+
+    float inclinação = Raio_base / Altura;
+
+    return normalizar(radial + Eixo * inclinação);
 }
