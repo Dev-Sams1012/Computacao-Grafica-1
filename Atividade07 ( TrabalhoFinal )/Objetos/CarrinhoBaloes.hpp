ef8f17f739e7a7e9e2ac7e1818406e762d9a1098
Dev-Sams1012 <samuel.augusto.abreu@gmail.com>
2026-01-25 17:52:21 -0300

    Refatorando Carrinho para trocar esfera por elipsoide e deixar código limpo.

--- Atividade07 ( TrabalhoFinal )/Objetos/CarrinhoBaloes.hpp
+++ Atividade07 ( TrabalhoFinal )/Objetos/CarrinhoBaloes.hpp
@@ -4,117 +4,157 @@
 #include "../../Classes/Objetos/ObjetoComplexo/ObjetoComplexo.hpp"
 #include "../../Classes/Objetos/Paralelepipedo/Paralelepipedo.hpp"
 #include "../../Classes/Objetos/Cilindro/Cilindro.hpp"
-#include "../../Classes/Objetos/Esfera/Esfera.hpp"
+#include "../../Classes/Objetos/Elipsoide/Elipsoide.hpp"
 #include "../../Classes/Utils/Matriz/Matriz4x4.hpp"
-#include <cmath>
 
 struct CarrinhoBaloes : ObjetoComplexo
 {
     Paralelepipedo *corpo;
-    Paralelepipedo *tampo; 
-    Cilindro *aroL, *aroR, *cuboL, *cuboR;
-    Cilindro *raiosL[8], *raiosR[8];
-    Cilindro *apoioTrasL, *apoioTrasR, *hasteL, *hasteR, *barraMao;
-    Cilindro *cordaoV, *cordaoG, *cordaoB;
-    Esfera *balaoV, *balaoG, *balaoB;
+    Paralelepipedo *tampo;
+
+    Cilindro *aroEsquerdo, *aroDireito;
+    Cilindro *cuboEsquerdo, *cuboDireito;
+    Cilindro *raiosEsquerdo[8], *raiosDireito[8];
+
+    Cilindro *apoioTraseiroE, *apoioTraseiroD;
+    Cilindro *hasteEsquerda, *hasteDireita, *barraConducao;
+
+    Cilindro *cordaoBalaoR, *cordaoBalaoG, *cordaoBalaoB;
+    Elipsoide *balaoVermelho, *balaoVerde, *balaoAzul;
 
     CarrinhoBaloes()
     {
-        Cor vermelho(0.8f, 0.1f, 0.1f), verde(0.1f, 0.7f, 0.1f), azul(0.1f, 0.1f, 0.8f);
-        Cor metal(0.4f, 0.4f, 0.4f), preto(0.05f, 0.05f, 0.05f), branco(0.9f, 0.9f, 0.9f);
-        Cor madeiraClara(0.9f, 0.8f, 0.6f);
+        Cor colVermelho(0.8f, 0.1f, 0.1f);
+        Cor colVerde(0.1f, 0.7f, 0.1f);
+        Cor colAzul(0.1f, 0.1f, 0.8f);
+        Cor colMetal(0.4f, 0.4f, 0.4f);
+        Cor colPreto(0.05f, 0.05f, 0.05f);
+        Cor colBranco(0.9f, 0.9f, 0.9f);
+        Cor colMadeira(0.9f, 0.8f, 0.6f);
 
-        float largCorpo = 0.6f, altCorpo = 0.5f, profCorpo = 0.8f;
-        float vaoLivre = 0.25f;
-        float espessuraTampo = 0.04f;
+        float corpoLargura = 0.6f, corpoAltura = 0.5f, corpoProfundidade = 0.8f;
+        float alturaVaoLivre = 0.25f, espessuraTampo = 0.04f;
+        float rodaRaio = 0.22f, rodaLargura = 0.06f, cuboRaio = 0.03f;
+        float balaoRaioXZ = 0.16f, balaoRaioY = 0.21f, cordaoComprimento = 1.1f;
 
-        // 1. CORPO
-        Ponto centroCorpo(0.0f, vaoLivre + (altCorpo / 2.0f), 0.0f);
-        corpo = new Paralelepipedo(largCorpo, profCorpo, altCorpo, centroCorpo, vermelho, vermelho, vermelho, 10);
+        Ponto posCorpo(0.0f, alturaVaoLivre + (corpoAltura / 2.0f), 0.0f);
+        corpo = new Paralelepipedo(corpoLargura, corpoProfundidade, corpoAltura, posCorpo, colVermelho, colVermelho, colVermelho, 10);
         adicionarComponente(corpo);
 
-        // 1.1 TAMPO
-        Ponto centroTampo(0.0f, vaoLivre + altCorpo + (espessuraTampo / 2.0f), 0.0f);
-        tampo = new Paralelepipedo(largCorpo + 0.1f, profCorpo + 0.1f, espessuraTampo, centroTampo, madeiraClara, madeiraClara, madeiraClara, 10);
+        Ponto posTampo(0.0f, alturaVaoLivre + corpoAltura + (espessuraTampo / 2.0f), 0.0f);
+        tampo = new Paralelepipedo(corpoLargura + 0.1f, corpoProfundidade + 0.1f, espessuraTampo, posTampo, colMadeira, colMadeira, colMadeira, 10);
         adicionarComponente(tampo);
 
-        // 2. RODAS FRONTAIS
-        float raioRoda = 0.22f, largRoda = 0.06f, raioCubo = 0.03f, zRoda = -profCorpo / 3.0f;
-        Vetor dirEixo(1.0f, 0.0f, 0.0f);
-        for (int lado : {-1, 1}) {
-            float xRoda = lado * (largCorpo / 2.0f + 0.04f);
-            Ponto centroRoda(xRoda, raioRoda, zRoda);
-            Ponto pBaseCil(centroRoda.Cord_x - (largRoda / 2.0f), centroRoda.Cord_y, centroRoda.Cord_z);
-            Cilindro* aro = new Cilindro(pBaseCil, raioRoda, raioRoda - 0.02f, largRoda, dirEixo, true, true, true, preto, preto, preto, 10);
-            Cilindro* cubo = new Cilindro(pBaseCil, raioCubo, 0.0f, largRoda, dirEixo, true, true, false, metal, metal, metal, 10);
-            if (lado == -1) { aroL = aro; cuboL = cubo; } else { aroR = aro; cuboR = cubo; }
-            adicionarComponente(aro); adicionarComponente(cubo);
-            for (int i = 0; i < 8; i++) {
-                float ang = i * (M_PI / 4.0f);
-                Vetor dR(0.0f, cos(ang), sin(ang));
-                Ponto pI(centroRoda.Cord_x, centroRoda.Cord_y + dR.Cord_y * raioCubo, centroRoda.Cord_z + dR.Cord_z * raioCubo);
-                Cilindro* raio = new Cilindro(pI, 0.008f, raioRoda - raioCubo - 0.01f, dR, true, true, metal, metal, metal, 10);
-                if (lado == -1) raiosL[i] = raio; else raiosR[i] = raio;
-                adicionarComponente(raio);
-            }
+        float offsetEixoX = corpoLargura / 2.0f + 0.04f;
+        float posicaoEixoZ = -corpoProfundidade / 3.0f;
+        Vetor direcaoEixo(1.0f, 0.0f, 0.0f);
+
+        Ponto centroRodaEsq(-offsetEixoX, rodaRaio, posicaoEixoZ);
+        Ponto baseCilindroEsq(centroRodaEsq.Cord_x - (rodaLargura / 2.0f), centroRodaEsq.Cord_y, centroRodaEsq.Cord_z);
+
+        aroEsquerdo = new Cilindro(baseCilindroEsq, rodaRaio, rodaRaio - 0.02f, rodaLargura, direcaoEixo, true, true, true, colPreto, colPreto, colPreto, 10);
+        cuboEsquerdo = new Cilindro(baseCilindroEsq, cuboRaio, 0.0f, rodaLargura, direcaoEixo, true, true, false, colMetal, colMetal, colMetal, 10);
+        adicionarComponente(aroEsquerdo);
+        adicionarComponente(cuboEsquerdo);
+
+        for (int i = 0; i < 8; i++)
+        {
+            float angulo = i * (M_PI / 4.0f);
+
+            Vetor dirRaio(0.0f, cos(angulo), sin(angulo));
+            Ponto posInicial(centroRodaEsq.Cord_x, centroRodaEsq.Cord_y + dirRaio.Cord_y * cuboRaio, centroRodaEsq.Cord_z + dirRaio.Cord_z * cuboRaio);
+
+            raiosEsquerdo[i] = new Cilindro(posInicial, 0.008f, rodaRaio - cuboRaio - 0.01f, dirRaio, true, true, colMetal, colMetal, colMetal, 10);
+
+            adicionarComponente(raiosEsquerdo[i]);
         }
 
-        // 3. APOIOS TRASEIROS
-        float zApoio = profCorpo / 2.0f - 0.1f;
-        apoioTrasL = new Cilindro(Ponto(-largCorpo/4.0f, 0.0f, zApoio), 0.025f, vaoLivre, Vetor(0,1,0), true, true, metal, metal, metal, 10);
-        apoioTrasR = new Cilindro(Ponto(largCorpo/4.0f, 0.0f, zApoio), 0.025f, vaoLivre, Vetor(0,1,0), true, true, metal, metal, metal, 10);
-        adicionarComponente(apoioTrasL); adicionarComponente(apoioTrasR);
+        Ponto centroRodaDir(offsetEixoX, rodaRaio, posicaoEixoZ);
+        Ponto baseCilindroDir(centroRodaDir.Cord_x - (rodaLargura / 2.0f), centroRodaDir.Cord_y, centroRodaDir.Cord_z);
 
-        // 4. HASTE
-        float angH = 0.7f; Vetor dH(0.0f, cos(angH), sin(angH)); float lenH = 0.45f;
-        Ponto pIL(-largCorpo/2.0f, vaoLivre + altCorpo, profCorpo/2.0f);
-        Ponto pIR(largCorpo/2.0f, vaoLivre + altCorpo, profCorpo/2.0f);
-        hasteL = new Cilindro(pIL, 0.015f, lenH, dH, true, true, metal, metal, metal, 10);
-        hasteR = new Cilindro(pIR, 0.015f, lenH, dH, true, true, metal, metal, metal, 10);
-        Ponto pFL(pIL.Cord_x, pIL.Cord_y + dH.Cord_y * lenH, pIL.Cord_z + dH.Cord_z * lenH);
-        barraMao = new Cilindro(Ponto(pFL.Cord_x - 0.05f, pFL.Cord_y, pFL.Cord_z), 0.025f, largCorpo + 0.1f, Vetor(1,0,0), true, true, preto, preto, preto, 10);
-        adicionarComponente(hasteL); adicionarComponente(hasteR); adicionarComponente(barraMao);
+        aroDireito = new Cilindro(baseCilindroDir, rodaRaio, rodaRaio - 0.02f, rodaLargura, direcaoEixo, true, true, true, colPreto, colPreto, colPreto, 10);
+        cuboDireito = new Cilindro(baseCilindroDir, cuboRaio, 0.0f, rodaLargura, direcaoEixo, true, true, false, colMetal, colMetal, colMetal, 10);
+        adicionarComponente(aroDireito);
+        adicionarComponente(cuboDireito);
 
-        // 5. BALÕES CORRIGIDOS
-        Ponto pFix(0.0f, vaoLivre + altCorpo + espessuraTampo, -profCorpo/2.0f + 0.1f);
-        float lenC = 1.1f, rB = 0.16f;
+        for (int i = 0; i < 8; i++)
+        {
+            float angulo = i * (M_PI / 4.0f);
 
-        auto criaBalao = [&](Vetor dir, Cor c, Cilindro*& cord, Esfera*& bal) {
-            dir = normalizar(dir);
-            
-            // Criar o cordão
-            cord = new Cilindro(pFix, 0.004f, lenC, dir, true, true, branco, branco, branco, 10);
-            
-            // Escala para formato de balão (Y maior que X e Z)
-            float escalaY = 1.4f;
-            float escalaXZ = 0.9f;
-            
-            // CRIAR NA ORIGEM (0,0,0) - Essencial para a escala não deslocar o objeto
-            bal = new Esfera(Ponto(0,0,0), rB, c, c, c, 50);
-            
-            // Matriz de Escala
-            Matriz4x4 S = Matriz4x4::escala(escalaXZ, escalaY, escalaXZ);
-            
-            // Posicionamento: Distância do cordão + metade da altura escalada da esfera
-            float distCentro = lenC + (rB * escalaY);
-            Ponto pCentro(pFix.Cord_x + dir.Cord_x * distCentro, 
-                          pFix.Cord_y + dir.Cord_y * distCentro, 
-                          pFix.Cord_z + dir.Cord_z * distCentro);
-            
-            // Matriz de Translação
-            Matriz4x4 T = Matriz4x4::translacao(pCentro.Cord_x, pCentro.Cord_y, pCentro.Cord_z);
-            
-            // Transforma: Primeiro escala (no centro), depois move para o topo do cordão
-            bal->transforma(T * S);
-        };
+            Vetor dirRaio(0.0f, cos(angulo), sin(angulo));
+            Ponto posInicial(centroRodaDir.Cord_x, centroRodaDir.Cord_y + dirRaio.Cord_y * cuboRaio, centroRodaDir.Cord_z + dirRaio.Cord_z * cuboRaio);
 
-        criaBalao(Vetor(-0.2f, 0.95f, 0.1f), vermelho, cordaoV, balaoV);
-        criaBalao(Vetor(0.0f, 1.0f, 0.0f), verde, cordaoG, balaoG);
-        criaBalao(Vetor(0.2f, 0.95f, -0.1f), azul, cordaoB, balaoB);
+            raiosDireito[i] = new Cilindro(posInicial, 0.008f, rodaRaio - cuboRaio - 0.01f, dirRaio, true, true, colMetal, colMetal, colMetal, 10);
 
-        adicionarComponente(cordaoV); adicionarComponente(balaoV);
-        adicionarComponente(cordaoG); adicionarComponente(balaoG);
-        adicionarComponente(cordaoB); adicionarComponente(balaoB);
+            adicionarComponente(raiosDireito[i]);
+        }
+
+        float posicaoApoioZ = corpoProfundidade / 2.0f - 0.1f;
+
+        apoioTraseiroE = new Cilindro(Ponto(-corpoLargura / 4.0f, 0.0f, posicaoApoioZ), 0.025f, alturaVaoLivre, Vetor(0, 1, 0), true, true, colMetal, colMetal, colMetal, 10);
+        apoioTraseiroD = new Cilindro(Ponto(corpoLargura / 4.0f, 0.0f, posicaoApoioZ), 0.025f, alturaVaoLivre, Vetor(0, 1, 0), true, true, colMetal, colMetal, colMetal, 10);
+
+        adicionarComponente(apoioTraseiroE);
+        adicionarComponente(apoioTraseiroD);
+
+        Vetor direcaoHaste(0.0f, cos(0.7f), sin(0.7f));
+        float comprimentoHaste = 0.45f;
+
+        Ponto baseHasteE(-corpoLargura / 2.0f, alturaVaoLivre + corpoAltura, corpoProfundidade / 2.0f);
+        Ponto baseHasteD(corpoLargura / 2.0f, alturaVaoLivre + corpoAltura, corpoProfundidade / 2.0f);
+
+        hasteEsquerda = new Cilindro(baseHasteE, 0.015f, comprimentoHaste, direcaoHaste, true, true, colMetal, colMetal, colMetal, 10);
+        hasteDireita = new Cilindro(baseHasteD, 0.015f, comprimentoHaste, direcaoHaste, true, true, colMetal, colMetal, colMetal, 10);
+
+        Ponto fimHasteE(baseHasteE.Cord_x, baseHasteE.Cord_y + direcaoHaste.Cord_y * comprimentoHaste, baseHasteE.Cord_z + direcaoHaste.Cord_z * comprimentoHaste);
+
+        barraConducao = new Cilindro(Ponto(fimHasteE.Cord_x - 0.05f, fimHasteE.Cord_y, fimHasteE.Cord_z), 0.025f, corpoLargura + 0.1f, Vetor(1, 0, 0), true, true, colPreto, colPreto, colPreto, 10);
+
+        adicionarComponente(hasteEsquerda);
+        adicionarComponente(hasteDireita);
+        adicionarComponente(barraConducao);
+
+        Ponto posicaoFixacaoBaloes(0.0f, alturaVaoLivre + corpoAltura + espessuraTampo, -corpoProfundidade / 2.0f + 0.1f);
+        float deslocamentoBalao = cordaoComprimento + balaoRaioY;
+
+        Vetor dirR = normalizar(Vetor(-0.2f, 0.95f, 0.1f));
+
+        cordaoBalaoR = new Cilindro(posicaoFixacaoBaloes, 0.004f, cordaoComprimento, dirR, true, true, colBranco, colBranco, colBranco, 10);
+
+        balaoVermelho = new Elipsoide(Ponto(0, 0, 0), balaoRaioXZ, balaoRaioY, balaoRaioXZ, colVermelho, colVermelho, colVermelho, 50);
+
+        Ponto centroR = ray(posicaoFixacaoBaloes, dirR, deslocamentoBalao);
+
+        balaoVermelho->transforma(Matriz4x4::translacao(centroR.Cord_x, centroR.Cord_y, centroR.Cord_z));
+
+        adicionarComponente(cordaoBalaoR);
+        adicionarComponente(balaoVermelho);
+
+        Vetor dirG = normalizar(Vetor(0.0f, 1.0f, 0.0f));
+
+        cordaoBalaoG = new Cilindro(posicaoFixacaoBaloes, 0.004f, cordaoComprimento, dirG, true, true, colBranco, colBranco, colBranco, 10);
+
+        balaoVerde = new Elipsoide(Ponto(0, 0, 0), balaoRaioXZ, balaoRaioY, balaoRaioXZ, colVerde, colVerde, colVerde, 50);
+
+        Ponto centroG = ray(posicaoFixacaoBaloes, dirG, deslocamentoBalao);
+
+        balaoVerde->transforma(Matriz4x4::translacao(centroG.Cord_x, centroG.Cord_y, centroG.Cord_z));
+
+        adicionarComponente(cordaoBalaoG);
+        adicionarComponente(balaoVerde);
+
+        Vetor dirB = normalizar(Vetor(0.2f, 0.95f, -0.1f));
+
+        cordaoBalaoB = new Cilindro(posicaoFixacaoBaloes, 0.004f, cordaoComprimento, dirB, true, true, colBranco, colBranco, colBranco, 10);
+
+        balaoAzul = new Elipsoide(Ponto(0, 0, 0), balaoRaioXZ, balaoRaioY, balaoRaioXZ, colAzul, colAzul, colAzul, 50);
+
+        Ponto centroB = ray(posicaoFixacaoBaloes, dirB, deslocamentoBalao);
+
+        balaoAzul->transforma(Matriz4x4::translacao(centroB.Cord_x, centroB.Cord_y, centroB.Cord_z));
+
+        adicionarComponente(cordaoBalaoB);
+        adicionarComponente(balaoAzul);
     }
 };
 
